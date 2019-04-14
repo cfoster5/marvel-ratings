be9974ccdacf010ee5b9111900c307a221962aad
cfoster5 <cfoster3204@gmail.com>
2019-04-14 10:32:31 -0500

    Added Angular Fire and Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -10,10 +13,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNybF5kRCKiB6TS0C4_IFxa8WbO5v57YQ",
+  authDomain: "marvelratings.firebaseapp.com",
+  databaseURL: "https://marvelratings.firebaseio.com",
+  projectId: "marvelratings",
+  storageBucket: "marvelratings.appspot.com",
+  messagingSenderId: "162316971349"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
